110222dbfbffb703b7305f22447750a352fdf886
khan-rabiul <krabiulislam2@gmail.com>
2024-06-19 20:36:39 +0600

    new examples added

--- Getters-and-Setters-Object/script.js
+++ Getters-and-Setters-Object/script.js
@@ -83,3 +83,100 @@ Encapsulation: They help encapsulate the internal representation of the properti
 Validation: Setters can include validation logic to ensure the property values are valid.
 Computed Properties: Getters can compute and return values dynamically.
 This approach helps maintain the integrity of the data within an object and provides a way to manage how properties are accessed and modified. */
+
+
+
+
+// getter = special method  that makes a property readable
+// setter = special method that makes a property writeable
+
+class Rectangle {
+    
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    set width(newWidth){
+        if(newWidth > 0) {
+            this._width = newWidth;
+        } else {
+            console.error('Width must be a positive number')
+        }
+    }
+
+    set height(newHeight){
+        if(newHeight > 0) {
+            this._height = newHeight;
+        } else {
+            console.error('Height must be a positive number')
+        }
+    }
+    // Setters are writeable not readable here comes getters methods
+
+    get width(){
+        return this._width;
+    }
+    get height(){
+        return this._height;
+    }
+    get area() {
+        return this._width * this._height;
+    }
+}
+
+
+const rectangle = new Rectangle(4, 6);
+console.log(rectangle);
+// we can access as object's property
+console.log(rectangle.area);
+
+
+class Student {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    set firstName(newFirstName) {
+        if(typeof newFirstName === 'string' && newFirstName.length > 0) {
+            this._firstName= newFirstName;
+        } else {
+            console.error('First name must not be an empty string');
+        }
+    }
+    set lastName(newLastName) {
+        if(typeof newLastName === 'string' && newLastName.length > 0) {
+            this._lastName = newLastName;
+        } else {
+            console.error('Last name must not be an empty string');
+        }
+    }
+    set age (newAge) {
+        if(typeof newAge === 'number' && newAge > 0){
+            this._age = newAge;
+        } else {
+            console.error('Age must be a positive number and greater than zero')
+        }
+    }
+
+
+    get firstName() {
+        return this._firstName;
+    }
+    get lastName(){
+        return this._lastName;
+    }
+    get fullName(){
+        return this._firstName + " " + this._lastName;
+    }
+    get age(){
+        return this._age;
+    }
+
+};
+
+const student1 = new Student ("Jannatul", "Nayem", 20 );
+console.log(student1.fullName);
+console.log(student1.age);
