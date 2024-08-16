2fef62b8aa72f3a79330ac9742ef03d33d664156
khan-rabiul <krabiulislam2@gmail.com>
2024-08-16 16:59:07 +0600

    radius and diameter calculation

--- Higeher-Order-Function/script.js
+++ Higeher-Order-Function/script.js
@@ -81,3 +81,55 @@ const maximumPrizeWinner = students3.reduce(function (max, currentStudent) {
 const maximumPrizeWinnerName = maximumPrizeWinner.name;
 console.log(maximumPrizeWinner);
 console.log(maximumPrizeWinnerName);
+
+
+
+// Higher Order Function
+
+// Higher order functions are functions that take one or more functions as arguments, or return a function as their result.
+
+
+// Want to calcualte the area and diameter of a circle
+
+// Traditional way
+const radius = [1, 2, 3];
+
+// Traditional step 1: function to calculate area of the circle
+const calculateArea = function (radius) {
+    const output = [];
+    for(let i = 0; i < radius.length; i++) {
+    output.push(Math.PI * radius[i] * radius[i]);
+    }
+    return output;
+}
+console.log(calculateArea(radius));
+// Traditional step 2: function to calculate diameter of the circle
+const calcualteDiameter = function(radius) {
+    const output = [];
+    for (let i = 0; i < radius.length; i++) {
+        output.push(2 * radius[i]);
+    }
+    return output;
+}
+console.log(calcualteDiameter(radius));
+
+// As logic and funciton is almost same so we can make the function reusable (Higher Order Function)
+
+// Logic 1: Calculate the area
+const area = function(radius) {
+    return Math.PI * Math.pow(radius, 2);
+}
+// Logic 2: Calculate the diameter
+const diameter = function (radius) {
+    return 2 *  radius;
+}
+// Making a reusalble function to calculate area, diameter, etc
+const calculate = function (radius, logic) {
+    const output = [];
+    for (let i = 0; i < radius.length; i++) {
+        output.push(logic(radius[i]));
+    }
+    return output;
+}
+console.log(calculate(radius, area));
+console.log(calculate(radius, diameter));
